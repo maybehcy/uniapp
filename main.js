03676a3da378f0fd0924ec87a9d159f591a52a47
黄昌盈 <2505105031@qq.com>
2022-03-06 11:04:03 +0800

    首页完成

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
 import App from './App'
 
+// 1.导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+// 请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  //loding的弹窗
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+// 响应拦截器
+$http.afterRequest = function() {
+  //关闭loding
+  uni.hideLoading()
+}
+
+//2. 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
